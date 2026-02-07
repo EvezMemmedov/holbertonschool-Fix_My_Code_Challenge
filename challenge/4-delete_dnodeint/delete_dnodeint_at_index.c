fe5cc3f8b5de3f04fbabbddc8419cdf5c8b55431
Evez Memmedov <evezm254@gmail.com>
2026-02-07 16:45:58 +0000

    Added

---
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -0,0 +1,37 @@
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *tmp;
+	unsigned int i = 0;
+
+	if (!head || !*head)
+		return (-1);
+
+	tmp = *head;
+
+	/* 0-cı indeks */
+	if (index == 0)
+	{
+		*head = tmp->next;
+		if (*head)
+			(*head)->prev = NULL;
+		free(tmp);
+		return (1);
+	}
+
+	/* digərləri */
+	while (tmp && i < index)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+
+	if (!tmp)
+		return (-1);
+
+	tmp->prev->next = tmp->next;
+	if (tmp->next)
+		tmp->next->prev = tmp->prev;
+
+	free(tmp);
+	return (1);
+}
